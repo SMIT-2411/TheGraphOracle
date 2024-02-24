b3dab38896926a9be91bde3575c1fdf4fc5512f3
Smit Patel <smit24@icloud.com>
2024-02-24 23:22:32 +0530

    Initial Commitr

---
+++ BFS_View.swift
@@ -0,0 +1,18 @@
+//
+//  BFS_View.swift
+//  TheGraphOracle
+//
+//  Created by Smit Patel on 24/02/24.
+//
+
+import SwiftUI
+
+struct BFS_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BFS_View()
+}
